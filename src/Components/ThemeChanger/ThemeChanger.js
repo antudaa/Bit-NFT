4300e741ec8cfe67f3aaa9dda176336781c41edf
Antu_Das <antuchandradasp9@gmail.com>
2022-12-26 11:58:00 -0800

    Completed basic project setup

---
+++ src/Components/ThemeChanger/ThemeChanger.js
@@ -0,0 +1,40 @@
+import React, { useEffect } from 'react';
+import { themeChange } from 'theme-change';
+
+const ThemeChanger = () => {
+
+    const themeValues = [
+        "light",
+        "dark",
+        "cupcake",
+        "corporate",
+        "retro",
+        "cyberpunk",
+        "fantasy",
+        "luxury",
+        "autumn",
+        "lemonade",
+        "night",
+        "coffee",
+        "winter"
+    ]
+    useEffect(() => {
+        themeChange(false)
+    })
+
+    return (
+        <>
+            <select className='text-primary text-center rounded-xl' data-choose-theme>
+                <option className='text-primary' option="" value="">Default Value</option>
+                {
+                    themeValues.map((value, i) => (
+                        <option className='text-primary' key={i} value={value.toLowerCase()}>{value}</option>
+                    ))
+                }
+
+            </select>
+        </>
+    );
+};
+
+export default ThemeChanger;
